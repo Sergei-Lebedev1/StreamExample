e2219871ebedf08acfe23ed9a1ea902f871b1e99
Sergei_Lebedev1 <pooshok420@gmail.com>
2024-10-29 15:09:29 +0300

    ok

--- src/main/java/org/StreamExample/Main.java
+++ src/main/java/org/StreamExample/Main.java
@@ -5,38 +5,28 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 //Упр.1. Создать ArrayList с числами.
 // Отфильтровать только четные числа,
-// потом возвести их вквадрат,
+// потом возвести их в квадрат,
 // потом оставить только первые три элемента и вывести в консоль.
 
-
-
-
-//Упр.4. Создать метод, который принимает ArrayList с числами,
-// создает stream и подсчитывает суммувсех чисел(метод reduce),
-// результат возвращает.
-// Покрыть тестами.
 public class Main {
     public static void main(String[] args) {
         Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        String [] str1 = {"Aaaaa" ,"Bb" ,"Cc" ,"Ddddd", "Gg"};
-        ArrayList <Integer> arr2 = new ArrayList<>(Arrays.asList(arr));
+        String[] str1 = {"яблоко", "апельсин", "банан", "киви", "арбуз"};
+        ArrayList<Integer> arr2 = new ArrayList<>(Arrays.asList(arr));
 //        ArrayList<String> list2 = new ArrayList<>(Arrays.asList(str1));
 //        ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(arr));
 //        System.out.println(list1);
 //        System.out.println(list2);
-//        System.out.println(countWords(str1));
+        System.out.println(countWords(str1));
 //        System.out.println(countInt(arr));
-        System.out.println(countArrList(arr2));
+//        System.out.println(countToReduce(arr2));
 
 //        Stream<Integer> stream = list1.stream();
 //        stream.filter(x -> x%2 == 0).map(x -> x * x ).limit(3).forEach(System.out::println);// (x -> System.out .println(x))
 
-
-
-//
 //        Stream<Integer> stream2 = list1.stream();
 //        Stream<Integer> stream3 = list1.stream();
-//        ;// (x -> System.out .println(x))
+//        // (x -> System.out .println(x))
 //        stream1.filter(x -> x > 5).map(x -> x +1).forEach(System.out::println);
 //        stream2.filter(x -> x > 5).limit(3).forEach(System.out::println);
 //        stream3.skip(3).forEach(System.out::println);
@@ -47,32 +37,34 @@ public class Main {
 
 
     }
- //Упр.2. Создать метод, который принимает массив строк,
+
+// Упр.2. Создать метод, который принимает массив строк,
 // создает stream и возвращает количество слов,
 // которые длиннее четырех символов. Покрыть тестами.
-    public static int countWords(String[]txt){
+    public static int countWords(String[] txt) {
         ArrayList<String> list2 = new ArrayList<>(Arrays.asList(txt));
         Stream<String> str1 = list2.stream();
-        long b = str1.filter(x -> x.length()>4).count();
+        long b = str1.filter(x -> x.length() >= 4).count();
         return (int) b;
     }
+
 // Упр.3. Создать метод, который принимает массив чисел,
 // создает стрим и подсчитывает, сколько в массиве чисел,
 // больше заданного значения (например > 5).
 // Покрыть тестами.
-    public static int countInt(Integer[]arr1){
-        Stream<Integer> stream1  = Arrays.stream(arr1);
-        long z = stream1.filter(x -> x > 5).count();
+    public static int countInt(Integer[] arr1) {
+        Stream<Integer> stream1 = Arrays.stream(arr1);
+        long z = stream1.filter(x -> x >= 5).count();
         return (int) z;
 
     }
 
-//Упр.4. Создать метод, который принимает ArrayList с числами,
-// создает stream и подсчитывает суммувсех чисел(метод reduce),
+// Упр.4. Создать метод, который принимает ArrayList с числами,
+// создает stream и подсчитывает сумму всех чисел(метод reduce),
 // результат возвращает.
 // Покрыть тестами.
-    public static int countArrList(ArrayList<Integer> txt){
-        Stream <Integer> stream2 = txt.stream();
+    public static int countToReduce(ArrayList<Integer> txt) {
+        Stream<Integer> stream2 = txt.stream();
         int res = stream2.distinct().reduce(0, (acc, x) -> acc + x);
         return res;
 
--- src/test/java/StreamExampleTest.java
+++ src/test/java/StreamExampleTest.java
@@ -9,55 +9,101 @@ import static org.StreamExample.Main.*;
 public class StreamExampleTest {
     @Test
     public void streamTest1() {
-        String [] mass1 = {"My","Words","my","rules"};
-        Assertions.assertEquals(2,countWords(mass1));
+        String[] mass1 = {"my", "words", "my", "rules"};
+        Assertions.assertEquals(2, countWords(mass1));
 
     }
+
     @Test
-    public void streamTest2(){
-        String [] mass2 = {};
-        Assertions.assertEquals(0,countWords(mass2));
+    public void streamTest2() {
+        String[] mass2 = {};
+        Assertions.assertEquals(0, countWords(mass2));
     }
+
     @Test
-    public void streamTest3(){
-        String [] mass3 = {"Word"};
-        Assertions.assertEquals(0,countWords(mass3));
+    public void streamTest3() {
+        String[] mass3 = {"word"};
+        Assertions.assertEquals(1, countWords(mass3));
 
     }
+
     @Test
-    public void streamTest4(){
-        String [] mass4 = {"Qwerty"};
-        Assertions.assertEquals(1,countWords(mass4));
+    public void streamTest4() {
+        String[] mass4 = {"qwerty"};
+        Assertions.assertEquals(1, countWords(mass4));
     }
 
     @Test
     public void streamTest5() {
-        Integer [] mass5 = {2,3,5,7,44,55};
-        Assertions.assertEquals(3,countInt(mass5));
+        Integer[] mass5 = {2, 3, 5, 7, 44, 55};
+        Assertions.assertEquals(4, countInt(mass5));
 
     }
-    @Test
-    public void streamTest6(){
-        Integer [] mass6 = {};
-        Assertions.assertEquals(0,countInt(mass6));
-    }
-    @Test
-    public void streamTest7(){
-        Integer [] mass7 = {5};
-        Assertions.assertEquals(0,countInt(mass7));
 
-    }
     @Test
-    public void streamTest8(){
-        Integer [] mass8 = {6};
-        Assertions.assertEquals(1,countInt(mass8));
-    }
-    @Test
-    public void streamTest9(){
-        Integer [] mass9 = {2,3,5,7,44,55};
-        ArrayList<Integer> arr3 = new ArrayList<>(Arrays.asList(mass9));
-        Assertions.assertEquals(116,countArrList(arr3));
+    public void streamTest6() {
+        Integer[] mass6 = {};
+        Assertions.assertEquals(0, countInt(mass6));
     }
 
+    @Test
+    public void streamTest7() {
+        Integer[] mass7 = {5};
+        Assertions.assertEquals(1, countInt(mass7));
+
+    }
+
+    @Test
+    public void streamTest8() {
+        Integer[] mass8 = {-6};
+        Assertions.assertEquals(0, countInt(mass8));
+    }
+
+    @Test
+    public void streamTest9() {
+        Integer[] mass9 = {0, 2, 4, 5, 6};
+        Assertions.assertEquals(2, countInt(mass9));
+
+    }
+
+    @Test
+    public void streamTest10() {
+        Integer[] mass10 = {2, 3, 5, 44, 56};
+        ArrayList<Integer> arr3 = new ArrayList<>(Arrays.asList(mass10));
+        Assertions.assertEquals(110, countToReduce(arr3));
+    }
+
+    @Test
+    public void streamTest11() {
+        Integer[] mass11 = {1, 2, 2, 3, 3, 3};
+        ArrayList<Integer> arr3 = new ArrayList<>(Arrays.asList(mass11));
+        Assertions.assertEquals(6, countToReduce(arr3));
+    }
+
+    @Test
+    public void streamTest12() {
+        Integer[] mass12 = {-1, -2, -3, -1, -3};
+        ArrayList<Integer> arr3 = new ArrayList<>(Arrays.asList(mass12));
+        Assertions.assertEquals(-6, countToReduce(arr3));
+    }
+
+    @Test
+    public void streamTest13() {
+        Integer[] mass13 = {};
+        ArrayList<Integer> arr3 = new ArrayList<>(Arrays.asList(mass13));
+        Assertions.assertEquals(0, countToReduce(arr3));
+    }
+
+    @Test
+    public void streamTest14() {
+        Integer[] mass14 = {0, 1, 2, 3, 1, 3};
+        ArrayList<Integer> arr3 = new ArrayList<>(Arrays.asList(mass14));
+        Assertions.assertEquals(6, countToReduce(arr3));
+    }
 
 }
+
+
+
+
+
